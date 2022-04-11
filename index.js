0da1583d6ae25d9c49916ea129533f1bc5975ee3
Théo Migeat <44432694+STM3900@users.noreply.github.com>
2022-04-11 11:23:13 +0200

    Simple select

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const port = 8080;
 
+const mysql = require("mysql");
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    dialect: "mysql",
+    host: process.env.DB_HOST,
+  }
+);
+
 app.get("/", (req, res) => {
   res.send("Api ok !");
+});
+
+app.get("/getinfos", (req, res) => {
+  try {
+    sequelize.authenticate();
+    sequelize.query("SELECT * FROM `infos`").then(([results, metadata]) => {
+      res.send(results);
+    });
+  } catch (error) {
+    console.error("Error at line selection : ", error);
+    res.send(error);
+  }
 });
 
 app.listen(port, () => {
